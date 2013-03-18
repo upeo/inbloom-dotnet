c407748ae6fa035f78529ad710017a48e67c8563
Munsifali Rashid <mun.rashid@gmail.com>
2013-03-17 20:06:16 -0400

    Added debug

---
+++ SampleWebApp/Controllers/DebugController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    public class DebugController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Debug";
+            ViewBag.SelectedNavClass = "debug";
+
+            return View();
+        }
+    }
+}
