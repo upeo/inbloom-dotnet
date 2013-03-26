4c6d8f59cfc633c5b64802406a659eee0e7f4cf4
Munsifali Rashid <mun.rashid@gmail.com>
2013-03-25 23:21:46 -0400

    Added try catch to authorize controller

--- SampleWebApp/Controllers/AuthorizeController.cs
+++ SampleWebApp/Controllers/AuthorizeController.cs
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+using System;
 using System.Web.Mvc;
 using SampleWebApp.Components;
 using inBloomApiLibrary;
@@ -24,49 +25,56 @@ namespace SampleWebApp.Controllers
     {
         public ActionResult Index(string code)
         {
-            // Check for a token in the session already, and if found, no action is required
-            if (!string.IsNullOrEmpty(SessionInfo.Current.AccessToken))
-                return RedirectToAction("Index", "Home");
-
-            // Init oAuth
-            var oAuth = new OAuth();
-
-            // We get a code back from the first leg of OAuth process.  If we don't have one, let's get it.
-            if (string.IsNullOrEmpty(code))
+            try
             {
-                string authorizationUrl = oAuth.GetAuthorizationUrl();
-                return Redirect(authorizationUrl);
-            }
+                // Check for a token in the session already, and if found, no action is required
+                if (!string.IsNullOrEmpty(SessionInfo.Current.AccessToken))
+                    return RedirectToAction("Index", "Home");
 
-            // Otherwise, we have a code, we can run the second leg of OAuth process.
-            string authorization = oAuth.CallAuthorization(null, code);
+                // Init oAuth
+                var oAuth = new OAuth();
 
-            // OAuth successful so get values, store in session and continue
-            if (authorization == "OAuthSuccess")
-            {
-                // Ensure that all required values were retrieved from the OAuth login
-                if (oAuth.AccessToken != null && oAuth.UserFullName != null && oAuth.UserSLIRoles != null && oAuth.UserId != null)
+                // We get a code back from the first leg of OAuth process.  If we don't have one, let's get it.
+                if (string.IsNullOrEmpty(code))
                 {
-                    // Authorization successful; set session variables
-                    SessionInfo.Current.AccessToken = oAuth.AccessToken;
-                    SessionInfo.Current.FullName = oAuth.UserFullName;
-                    SessionInfo.Current.Roles = oAuth.UserSLIRoles;
-                    SessionInfo.Current.UserId = oAuth.UserId;
-
-                    // Redirect to post login URL if one exists
-                    if (!string.IsNullOrEmpty(SessionInfo.Current.PostLoginRedirectUrl))
-                    {
-                        var returnUrl = SessionInfo.Current.PostLoginRedirectUrl;
-                        SessionInfo.Current.PostLoginRedirectUrl = null;
-                        return Redirect(returnUrl);
-                    }
-
-                    // Otherwise, just go to dashboard
-                    return RedirectToAction("Index", "Dashboard");
+                    string authorizationUrl = oAuth.GetAuthorizationUrl();
+                    return Redirect(authorizationUrl);
                 }
-            }
 
-            return Content("Error authorizing");
+                // Otherwise, we have a code, we can run the second leg of OAuth process.
+                string authorization = oAuth.CallAuthorization(null, code);
+
+                // OAuth successful so get values, store in session and continue
+                if (authorization == "OAuthSuccess")
+                {
+                    // Ensure that all required values were retrieved from the OAuth login
+                    if (oAuth.AccessToken != null && oAuth.UserFullName != null && oAuth.UserSLIRoles != null && oAuth.UserId != null)
+                    {
+                        // Authorization successful; set session variables
+                        SessionInfo.Current.AccessToken = oAuth.AccessToken;
+                        SessionInfo.Current.FullName = oAuth.UserFullName;
+                        SessionInfo.Current.Roles = oAuth.UserSLIRoles;
+                        SessionInfo.Current.UserId = oAuth.UserId;
+
+                        // Redirect to post login URL if one exists
+                        if (!string.IsNullOrEmpty(SessionInfo.Current.PostLoginRedirectUrl))
+                        {
+                            var returnUrl = SessionInfo.Current.PostLoginRedirectUrl;
+                            SessionInfo.Current.PostLoginRedirectUrl = null;
+                            return Redirect(returnUrl);
+                        }
+
+                        // Otherwise, just go to home
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                return Content("Unknown Error authorizing");
+            }
+            catch (Exception ex)
+            {
+                return Content("Error authorizing: " + ex.Message);
+            }
         }
 
         public ActionResult LogOut()
