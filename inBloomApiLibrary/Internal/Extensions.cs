1561570098e7c8e58d76b867036e5527a26b9353
Greg Enslow <greg.enslow@enslowsolutions.com>
2013-11-25 17:55:01 -0800

    Adding Uri extension method.
    Adds support for adding query string params.

---
+++ inBloomApiLibrary/Internal/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace inBloomApiLibrary
+{
+    public static class Extensions
+    {
+        public static Uri AddQuery(this Uri uri, string name, string value)
+        {
+            var ub = new UriBuilder(uri);
+
+            // decodes urlencoded pairs from uri.Query to HttpValueCollection
+            var httpValueCollection = HttpUtility.ParseQueryString(uri.Query);
+
+            httpValueCollection.Add(name, value);
+
+            // urlencodes the whole HttpValueCollection
+            ub.Query = httpValueCollection.ToString();
+
+            return ub.Uri;
+        }
+    }
+}
