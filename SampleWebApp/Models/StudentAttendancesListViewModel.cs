e31f057628b12bbe64f178bbc8045c62e62f736d
Greg Enslow <greg.enslow@enslowsolutions.com>
2013-12-18 18:05:48 -0800

    Adding attendance and assessment view models.

---
+++ SampleWebApp/Models/StudentAttendancesListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SampleWebApp.Models
+{
+    public class StudentAttendancesListViewModel
+    {
+        public JArray StudentAttendances { get; set; }
+    }
+}
